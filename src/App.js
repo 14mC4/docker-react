ce208f696ee8077447887f7f04056404bafb0617
yrroth <charles.goodling@gmail.com>
2021-04-15 14:18:55 +0000

    test 701

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Testing Testing.LOL
+          Who let the dogs out?
+          Where are my Dogs at?
         </p>
         <a
           className="App-link"
