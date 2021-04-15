3a711e9c92458cdf64c69a2e52f67578d695568f
yrroth <charles.goodling@gmail.com>
2021-04-15 22:53:36 +0000

    changed app text

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
           Who let the dogs out?
           Where are my Dogs at?
+          I was changed on the Feature Branch
         </p>
         <a
           className="App-link"
