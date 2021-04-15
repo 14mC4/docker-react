d00749c2010b455db06b7bfe4bae6e0a4791db26
yrroth <charles.goodling@gmail.com>
2021-04-15 13:52:26 +0000

    fixing request issue with travis unable to parse

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Testing Testing
+          Testing Testing.LOL
         </p>
         <a
           className="App-link"
